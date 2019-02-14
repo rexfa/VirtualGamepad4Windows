12e4dee99aba99d982a216cf201cda4335a34891
rexfa <rex_fa@163.com>
2019-02-14 18:27:32 +0800

    建立工程

---
+++ VirtualGamepad4Windows/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace VirtualGamepad4Windows
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+        }
+    }
+}
