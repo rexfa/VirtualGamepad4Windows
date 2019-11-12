7822413a22057234b5c2b524e6aff5fff5f5f6ef
rexfa <rex_fa@hotmail.com>
2019-11-12 23:49:53 +0800

    抄写DS4

---
+++ VirtualGamepad4Windows/VGControl/ITouchpadBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VirtualGamepad4Windows
+{
+    interface ITouchpadBehaviour
+    {
+        void touchesBegan(object sender, TouchpadEventArgs arg);
+        void touchesMoved(object sender, TouchpadEventArgs arg);
+        void touchButtonUp(object sender, TouchpadEventArgs arg);
+        void touchButtonDown(object sender, TouchpadEventArgs arg);
+        void touchesEnded(object sender, TouchpadEventArgs arg);
+        void sixaxisMoved(object sender, SixAxisEventArgs unused);
+        void touchUnchanged(object sender, EventArgs unused);
+    }
+}
